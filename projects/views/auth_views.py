94b02590a6660f6e101e1cd8479ef86d1598a81d
mafeorozco <mariafernandaorozco886@gmail.com>
2025-04-17 15:01:49 -0500

    uptade views files

--- projects/views/auth_views.py
+++ projects/views/auth_views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets,permissions, status
-from .serializers import UserSerializer
+from ..serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -34,12 +34,3 @@ def register(request):
         token = Token.objects.create(user=user)
         return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def getUsers(request):
-    users = User.objects.all()
-    print(users)
-    serializer = UserSerializer(users, many=True)
-    return Response({"usuarios": serializer.data})
